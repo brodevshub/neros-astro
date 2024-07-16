ce6125269b01a77f5ee029dd070b35ed7b460551
colmeneroHub <colmenerosanchezmario@gmail.com>
2024-07-16 19:40:36 +0200

    adaptador vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import dotenv from 'dotenv';
-
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
