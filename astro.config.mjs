dcffbc01ad343ddb65cf0aa076ab46834d2bb091
colmeneroHub <colmenerosanchezmario@gmail.com>
2024-07-23 18:18:08 +0200

    sitemap & robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import dotenv from 'dotenv';
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
+  site: 'https://fisioterapianeros.com',
+  integrations: [react(), sitemap()],
   adapter: vercel()
 });
